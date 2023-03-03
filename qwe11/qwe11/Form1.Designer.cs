6cacb6c669ab1e7a10e55ddaa5665cfdaf72a89c
podsvinok <126859890+podsvinok@users.noreply.github.com>
2023-03-03 13:06:19 +0500

    Add files via upload

---
+++ qwe11/qwe11/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace qwe11
+{
+    partial class Tester
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tester));
+            this.var1 = new System.Windows.Forms.CheckBox();
+            this.var2 = new System.Windows.Forms.CheckBox();
+            this.var3 = new System.Windows.Forms.CheckBox();
+            this.var4 = new System.Windows.Forms.CheckBox();
+            this.question = new System.Windows.Forms.Label();
+            this.answer = new System.Windows.Forms.Button();
+            this.testchoice = new System.Windows.Forms.Button();
+            this.quantity = new System.Windows.Forms.TrackBar();
+            this.accept = new System.Windows.Forms.Button();
+            this.quantityScroll = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.quantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // var1
+            // 
+            resources.ApplyResources(this.var1, "var1");
+            this.var1.Name = "var1";
+            this.var1.UseVisualStyleBackColor = true;
+            this.var1.Click += new System.EventHandler(this.var_Click);
+            // 
+            // var2
+            // 
+            resources.ApplyResources(this.var2, "var2");
+            this.var2.Name = "var2";
+            this.var2.UseVisualStyleBackColor = true;
+            this.var2.Click += new System.EventHandler(this.var_Click);
+            // 
+            // var3
+            // 
+            resources.ApplyResources(this.var3, "var3");
+            this.var3.Name = "var3";
+            this.var3.UseVisualStyleBackColor = true;
+            this.var3.Click += new System.EventHandler(this.var_Click);
+            // 
+            // var4
+            // 
+            resources.ApplyResources(this.var4, "var4");
+            this.var4.Name = "var4";
+            this.var4.UseVisualStyleBackColor = true;
+            this.var4.Click += new System.EventHandler(this.var_Click);
+            // 
+            // question
+            // 
+            resources.ApplyResources(this.question, "question");
+            this.question.Name = "question";
+            this.question.UseWaitCursor = true;
+            // 
+            // answer
+            // 
+            resources.ApplyResources(this.answer, "answer");
+            this.answer.Name = "answer";
+            this.answer.UseVisualStyleBackColor = true;
+            this.answer.Click += new System.EventHandler(this.answer_Click);
+            // 
+            // testchoice
+            // 
+            resources.ApplyResources(this.testchoice, "testchoice");
+            this.testchoice.Name = "testchoice";
+            this.testchoice.UseVisualStyleBackColor = true;
+            this.testchoice.Click += new System.EventHandler(this.testchoice_Click);
+            // 
+            // quantity
+            // 
+            this.quantity.LargeChange = 1;
+            resources.ApplyResources(this.quantity, "quantity");
+            this.quantity.Minimum = 1;
+            this.quantity.Name = "quantity";
+            this.quantity.Value = 1;
+            this.quantity.ValueChanged += new System.EventHandler(this.quantity_Changed);
+            // 
+            // accept
+            // 
+            resources.ApplyResources(this.accept, "accept");
+            this.accept.Name = "accept";
+            this.accept.UseVisualStyleBackColor = true;
+            this.accept.Click += new System.EventHandler(this.accept_Click);
+            // 
+            // quantityScroll
+            // 
+            resources.ApplyResources(this.quantityScroll, "quantityScroll");
+            this.quantityScroll.Name = "quantityScroll";
+            // 
+            // Tester
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.quantityScroll);
+            this.Controls.Add(this.accept);
+            this.Controls.Add(this.quantity);
+            this.Controls.Add(this.testchoice);
+            this.Controls.Add(this.answer);
+            this.Controls.Add(this.question);
+            this.Controls.Add(this.var4);
+            this.Controls.Add(this.var3);
+            this.Controls.Add(this.var2);
+            this.Controls.Add(this.var1);
+            this.Name = "Tester";
+            ((System.ComponentModel.ISupportInitialize)(this.quantity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox var1;
+        private System.Windows.Forms.CheckBox var3;
+        private System.Windows.Forms.CheckBox var4;
+        private System.Windows.Forms.CheckBox var2;
+        private System.Windows.Forms.Label question;
+        private System.Windows.Forms.Button answer;
+        private System.Windows.Forms.Button testchoice;
+        private System.Windows.Forms.TrackBar quantity;
+        private System.Windows.Forms.Button accept;
+        private System.Windows.Forms.Label quantityScroll;
+    }
+}
+
---
+++ qwe11/qwe11/Form1.cs
@@ -0,0 +1,130 @@
+﻿//1.Реализовать программу - тест, в которой будет предусмотрено
+//любое количество вопросов и четыре варианта ответов.
+//Причем можно выбрать несколько вариантов ответов,
+//или все (но нельзя ничего не выбирать). До тех пор,
+//пока не выбран ни один из вариантов ответов, переход
+//к следующему вопросу невозможен. Данные о вопросах
+//хранятся в файле. Программа должна в конце теста
+//создавать файл с выбранными вариантами ответов.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+namespace qwe11
+{
+    public partial class Tester : Form
+    {
+        StreamReader reader;
+        private string[] answers;
+        private int quest, rightAnswers;
+        private string rightAnswer;
+        public Tester()
+        {
+            InitializeComponent();
+            quantity.Scroll += quantity_Scroll;
+        }
+
+        private void quantity_Scroll(object sender, EventArgs e) { quantityScroll.Text = String.Format("Кол-во вопросов: {0}", quantity.Value); }
+        private void quantity_Changed(object sender, EventArgs e) { accept.Enabled = true; }
+
+        private void testchoice_Click(object sender, EventArgs e)
+        {
+            quantityScroll.Text = "Кол-во вопросов: 1";
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = "c:\\",
+                Filter = "Text files (*.txt)|*.txt",
+                RestoreDirectory = true
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                reader = new StreamReader(openFileDialog.FileName);
+                quantity.Visible = true; quantityScroll.Visible = true; quantity.Maximum = Convert.ToInt16(reader.ReadLine());
+                accept.Visible = true;
+            }
+        }
+
+        private void accept_Click(object sender, EventArgs e)
+        {
+            question.Visible = true;
+            quantity.Visible = false; quantityScroll.Visible = false;
+            testchoice.Visible = false;
+            accept.Visible = false;
+            var1.Visible = true; var2.Visible = true; var3.Visible = true; var4.Visible = true;
+            answer.Visible = true;
+            answers = new string[quantity.Value];
+            toquest();
+        }
+
+        private void toquest()
+        {
+            if (quest < quantity.Value)
+            {
+                question.Text = reader.ReadLine();
+                var1.Text = reader.ReadLine();
+                var2.Text = reader.ReadLine();
+                var3.Text = reader.ReadLine();
+                var4.Text = reader.ReadLine();
+                rightAnswer = reader.ReadLine();
+            }
+        }
+
+        private void var_Click(object sender, EventArgs e)
+        {
+            if (var1.Checked || var2.Checked || var3.Checked || var4.Checked) answer.Enabled = true;
+            else answer.Enabled = false;
+        }
+
+        private void answer_Click(object sender, EventArgs e)
+        {
+            if (var1.Checked) { answers[quest] += "1"; var1.Checked = false; }
+            if (var2.Checked) { answers[quest] += "2"; var2.Checked = false; }
+            if (var3.Checked) { answers[quest] += "3"; var3.Checked = false; }
+            if (var4.Checked) { answers[quest] += "4"; var4.Checked = false; }
+
+            if (rightAnswer == answers[quest]) rightAnswers += 1;
+
+            if (quest < quantity.Value - 1)
+            {
+                quest += 1;
+                toquest();
+            }
+            else
+            {
+                MessageBox.Show("Правильных ответов: " + rightAnswers + " из " + quantity.Value);
+                reader.Close();
+                reader.Dispose();
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    InitialDirectory = "c:\\",
+                    Filter = "Text files (*.txt)|*.txt",
+                    RestoreDirectory = true
+                };
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName, false))
+                    {
+                        for (int i = 0; i < quantity.Value; i++)
+                        {
+                            writer.WriteLine(answers[i]);
+                        }
+                    }
+                }
+                testchoice.Visible = true;
+                var1.Visible = false; var2.Visible = false; var3.Visible = false; var4.Visible = false;
+                answer.Visible = false; question.Visible = false;
+                rightAnswers = 0; quest = 0;
+            }
+        }
+    }
+}
